72bc742297da5b1821f7d29050cb5656e3adf64b
djordje_radivojevic_eng <djordje.radivojevic.eng@gmail.com>
2020-11-11 08:13:57 +0100

    Initial commit

---
+++ src/test/java/com/springframework/spring5jmsexamples/Spring5JmsExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.spring5jmsexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JmsExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
